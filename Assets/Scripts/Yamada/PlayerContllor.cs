ad7a9b08d672379ae33efc59a988efce8bf47609
YamadaHibiki430 <hibisyun430@gmail.com>
2022-03-01 11:44:21 +0900

    プレイヤーコントローラーの整備

--- Assets/Scripts/Yamada/PlayerContllor.cs
+++ Assets/Scripts/Yamada/PlayerContllor.cs
@@ -2,142 +2,158 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+/// <summary>
+/// プレイヤーのコントローラー
+/// </summary>
 public class PlayerContllor : MonoBehaviour
 {
+    //アニメーター
     [SerializeField]
-    private Animator playerAnimator = null;
-    [SerializeField, Header("HPUI")]
-    private Slider HPUI;
+    private Animator animator = null;
+
+    //体力バー
+    [SerializeField]
+    private Slider hpBar;
+
+    //シールド
     [SerializeField]
     private GameObject shieldObject = null;
+
     [SerializeField, Header("プレイヤーの最大ライフポイント")]
-    public int maxLifePoint = 5;
+    private int maxLifePoint = 5;
+
     [SerializeField, Header("プレイヤーのライフポイント")]
-    public int lifePoint = 5;
+    private int lifePoint = 5;
+
     [SerializeField, Header("プレイヤーの死亡判定")]
-    public bool isDead = false;
-    //ウィザードの移動制限
+    private bool isDead = false;
+
+    [SerializeField, Header("プレイヤーの移動速度")]
+    private float moveSpeed = 50.0f;
+
+    [SerializeField, Header("プレイヤーの回転速度")]
+    private float rotateSpeed = 25.0f;
+
+    private Vector3 moveAngle = Vector3.zero;
+    #region プレイヤーの移動制限
     [SerializeField]
     private float maxLimitPositionZ = 5.0f;
+
     [SerializeField]
     private float minLimitPositionZ = -5.0f;
+
     [SerializeField]
     private float maxLimitPositionX = 5.0f;
+
     [SerializeField]
     private float minLimitPositionX = -5.0f;
+    #endregion
+
     private void Start()
     {
-        HPUI.value = maxLifePoint;
+        hpBar.value = maxLifePoint;
     }
 
-    void Update()
+    private void Update()
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
             shieldObject.SetActive(true);
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.01f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 0.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsDush", true);
+                MoveCharacter(Vector3.forward, rotateSpeed, "IsWalk");
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.01f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 180.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsDush", true);
+                MoveCharacter(Vector3.back, rotateSpeed, "IsWalk");
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.01f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 90.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsDush", true);
+                MoveCharacter(Vector3.right, rotateSpeed, "IsWalk");
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.01f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = -90.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsDush", true);
-            }
-            else
-            {
-                playerAnimator.SetBool("IsSprint", false);
-                playerAnimator.SetBool("IsDush", false);
+                MoveCharacter(Vector3.left, rotateSpeed, "IsWalk");
+
             }
         }
         else
         {
             shieldObject.SetActive(false);
+
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.02f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 0.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsSprint", true);
+                MoveCharacter(Vector3.forward, rotateSpeed, "IsDash");
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.02f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 180.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsSprint", true);
+                MoveCharacter(Vector3.back, rotateSpeed, "IsDash");
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.02f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = 90.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsSprint", true);
+                MoveCharacter(Vector3.right, rotateSpeed, "IsDash");
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
-                this.transform.Translate(0.0f, 0.0f, 0.02f);
-                Vector3 angle = this.transform.eulerAngles;
-                angle.y = -90.0f;
-                this.transform.eulerAngles = angle;
-                playerAnimator.SetBool("IsSprint", true);
-            }
-            else
-            {
-                playerAnimator.SetBool("IsSprint", false);
-                playerAnimator.SetBool("IsDush", false);
+                MoveCharacter(Vector3.left, rotateSpeed, "IsDash");
             }
         }
-        if (transform.position.x <= minLimitPositionX)
+
+        if (!Input.anyKey)
         {
-            transform.position = new Vector3(minLimitPositionX, transform.position.y, transform.position.z);
+            moveAngle = Vector3.zero;
         }
-        if (transform.position.x >= maxLimitPositionX)
+
+        if(Vector3.zero == moveAngle)
         {
-            transform.position = new Vector3(maxLimitPositionX, transform.position.y, transform.position.z);
+            animator.SetBool("IsDash", false);
+            animator.SetBool("IsWalk", false);
         }
-        if (transform.position.z <= minLimitPositionZ)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, minLimitPositionZ);
-        }
-        if (transform.position.z >= maxLimitPositionZ)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, maxLimitPositionZ);
-        }
-        HPUI.value = lifePoint;
+
+        MoveLimit(maxLimitPositionX, minLimitPositionX, maxLimitPositionZ, minLimitPositionZ);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Bullet")
+        if (collision.gameObject.CompareTag("Bullet"))
         {
             lifePoint--;
+            hpBar.value = lifePoint;
         }
+    }
+
+    /// <summary>
+    /// キャラクターの移動関数
+    /// </summary>
+    /// <param name="moveDirection"></param>
+    /// <param name="rotateSpeed"></param>
+    /// <param name="playAnime"></param>
+    private void MoveCharacter(Vector3 moveDirection ,float rotateSpeed, string playAnime)
+    {
+        moveAngle = moveDirection;
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        var look = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, look, Time.deltaTime * rotateSpeed);
+
+        animator.SetBool(playAnime, true);
+    }
+
+    /// <summary>
+    /// プレイヤーの移動制限
+    /// </summary>
+    /// <param name="maxLimitX"></param>
+    /// <param name="minLimitX"></param>
+    /// <param name="maxLimitZ"></param>
+    /// <param name="minLimitZ"></param>
+    private void MoveLimit(float maxLimitX, float minLimitX, float maxLimitZ, float minLimitZ)
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minLimitX, maxLimitX),transform.position.y,
+                                         Mathf.Clamp(transform.position.z, minLimitZ, maxLimitZ));
     }
 }
